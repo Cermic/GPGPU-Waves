ef0af275e534fbe0db4bf54825e2f3667c28a251
Jack Smith <streamlined50@hotmail.com>
2018-10-22 19:46:14 +0100

    Minor changes to the jitter kernel - now doesn't override the last one but still not adding the jitter correctly yet.

--- wave.cu
+++ wave.cu
@@ -180,18 +180,11 @@ __global__ void jitter_kernel(float4 *pos, unsigned int width, unsigned int heig
 	unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
 
-	// calculate uv coordinates
-	float u = x / (float)width;
-	float v = y / (float)height;
-	u = u*2.0f - 1.0f;
-	v = v*2.0f - 1.0f;
-
-	// calculate simple sine wave pattern
-	float freq = 3.0f;
-	float w = sinf(u*freq + time) * cosf(v*freq + time) * 0.5f;
-
 	// write output vertex
-	pos[y*width + x] = make_float4(u, w, v, 1.0f);
+	pos[y*width + x].x += 0.1f;
+	pos[y*width + x].y += 0.1f;
+	pos[y*width + x].z += 0.1f;
+
 }
 
 
@@ -202,8 +195,11 @@ void launch_kernel(float4 *pos, unsigned int mesh_width,
     dim3 block(8, 8, 1);
     dim3 grid(mesh_width / block.x, mesh_height / block.y, 1);
     simple_vbo_kernel<<< grid, block>>>(pos, mesh_width, mesh_height, time, offset);
-	if(jitter_on)
+	//cudaDeviceSynchronize();
+	if (jitter_on)
+	{
 		jitter_kernel << < grid, block >> > (pos, mesh_width, mesh_height, time);
+	}
 }
 
 bool checkHW(char *name, const char *gpuType, int dev)
